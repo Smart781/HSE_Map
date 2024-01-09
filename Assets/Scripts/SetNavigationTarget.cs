81d89a4ae047d5244f8ead43a9eb8bf6340ac7e1
Smart781 <84939218+Smart781@users.noreply.github.com>
2024-01-09 14:00:39 +0300

    Добавление нескольких целей, кнопки выбора и расчета маршрута.

--- Assets/Scripts/SetNavigationTarget.cs
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -1,37 +1,46 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class SetNavigationTarget : MonoBehaviour
-{
+public class SetNavigationTarget : MonoBehaviour {
     [SerializeField]
-    private Camera topDownCamera;
+    private TMP_Dropdown navigationTargetDropDown;
     [SerializeField]
-    private GameObject navTargetObject;
-
-    private UnityEngine.AI.NavMeshPath path;
+    private List<Target> navigationTargetObjects = new List<Target>();
+    
+    private NavMeshPath path;
     private LineRenderer line;
+    private Vector3 targetPosition = Vector3.zero;
 
     private bool lineToggle = false;
 
-    void Start()
-    {
-         path = new UnityEngine.AI.NavMeshPath();
+    private void Start() {
+         path = new NavMeshPath();
          line = transform.GetComponent<LineRenderer>();
+         line.enabled = lineToggle;
         
     }
 
-    void Update()
-    {
-        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)) {
-            lineToggle = !lineToggle;
-        }
-
-        if (lineToggle) {
-            UnityEngine.AI.NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, UnityEngine.AI.NavMesh.AllAreas, path);
+    private void Update() {
+        if (lineToggle && targetPosition != Vector3.zero) {
+            NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
             line.positionCount = path.corners.Length;
             line.SetPositions(path.corners);
-            line.enabled = true;
         }
+    }
+
+    public void SetCurrentNavigationTarget(int selectedValue) {
+        targetPosition = Vector3.zero;
+        string selectedText = navigationTargetDropDown.options[selectedValue].text;
+        Target currentTarget = navigationTargetObjects.Find(x => x.Name.Equals(selectedText));
+        if (currentTarget != null) {
+            targetPosition = currentTarget.PositionObject.transform.position;
+        }
+    }
+
+    public void ToogleVisiblity() {
+        lineToggle = !lineToggle;
+        line.enabled = lineToggle;
     }
 }
